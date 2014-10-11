1e07ee57478f87dfb819c3e84349915aa4728575
Matt <matt.ho@gmail.com>
2014-10-11 10:20:14 -0700

    - added velocity library to handle css transitions - web pages reloaded automatically - added basic style

--- server/handlers.go
+++ server/handlers.go
@@ -4,18 +4,28 @@ import (
 	"github.com/soveran/redisurl"
 	"html/template"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"time"
 )
 
+func LoadTemplate(docroot, path string) *template.Template {
+	log.Printf("loading template, %s\n", path)
+	return template.Must(template.ParseFiles(docroot + path))
+}
+
 func Server(docroot string) http.HandlerFunc {
 	fileServer := http.FileServer(http.Dir(docroot))
-	home := template.Must(template.ParseFiles(docroot + "/index.html"))
+	home := LoadTemplate(docroot, "/index.html")
 
 	return func(w http.ResponseWriter, req *http.Request) {
 		switch req.RequestURI {
 		case "/", "/index.html":
+			if !cfg.IsProduction {
+				// in development mode reload the template
+				home = LoadTemplate(docroot, "/index.html")
+			}
 			home.Execute(w, cfg)
 		default:
 			fileServer.ServeHTTP(w, req)
--- src/main/core.cljs
+++ src/main/core.cljs
@@ -4,8 +4,22 @@
 
 (enable-console-print!)
 
+; ------------------------------------------------------------------------
+
+(defn activate-page [id]
+  (let [element (.getElementById js/document id)]
+    (println "activating page," id)
+    (set! (.-className element) (str (.-className element) " hero-active"))
+    (js/Velocity element "transition.slideRightBigIn" 250)))
+
+; ------------------------------------------------------------------------
+
 (defn app-view []
-  [:div [:div.title [:h1 "Hello World"]]])
+  [:div [:div.title [:h1 "Hello World"]]
+   [:p#sample.hero-page "this is a page"]
+   [:input {:type "button"
+            :on-click #(activate-page "sample")
+            :value "click me"}]])
 
 (reagent/render-component [app-view] (.getElementById js/document "app"))
 
